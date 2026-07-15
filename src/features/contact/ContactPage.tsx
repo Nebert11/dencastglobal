bce8f0515e7fcbf78e130ecf183dd457e69074b6
Nebert Kuria <nebert.kuria@ogilvy.africa>
2026-07-15 16:28:35 +0300

    New changes

--- src/features/contact/ContactPage.tsx
+++ src/features/contact/ContactPage.tsx
@@ -281,7 +281,7 @@ const ContactPage: React.FC = () => {
       <section className="h-80 bg-slate-200 relative overflow-hidden">
         <iframe
           title="Dencast Global Office Location"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127340.50432752424!2d-0.26953855!3d5.6036988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9084b2b7a773%3A0xbed14ed8650e2dd3!2sAccra%2C%20Ghana!5e0!3m2!1sen!2s!4v1600000000000!5m2!1sen!2s"
+          src="https://www.google.com/maps?q=Kenya+ParkView+Suites+Nyerere+Road+Nairobi&output=embed"
           className="w-full h-full border-0"
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
